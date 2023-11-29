34e071dbad7efa7d11baa3865eed4920e0037acd
Matheus Henrique <matheus.181137@gmail.com>
2023-11-29 10:41:15 -0300

    chore: project bootstrap

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import AppHeader from './components/Header/AppHeader.vue'
+</script>
+
+<template>
+  <AppHeader />
+</template>
---
+++ src/components/Header/AppHeader.vue
@@ -0,0 +1,29 @@
+<script setup>
+import TransfeeraIcon from '../icons/TransfeeraIcon.vue'
+</script>
+
+<template>
+  <header class="layout-spacing">
+    <i>
+      <TransfeeraIcon />
+    </i>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 2rem;
+}
+
+i {
+  display: flex;
+  place-items: center;
+  place-content: center;
+  width: 128px;
+  height: 25px;
+}
+</style>
