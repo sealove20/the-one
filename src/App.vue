006560190a0fc03b7c45c7ae73d11aa719d620b4
Matheus Henrique <matheus.181137@gmail.com>
2023-11-30 16:48:18 -0300

    feat: create navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import AppHeader from './components/Header/AppHeader.vue'
+import AppNavigation from './components/Navigation/AppNavigation.vue'
 </script>
 
 <template>
   <AppHeader />
+  <AppNavigation />
 </template>
---
+++ src/components/Navigation/AppNavigation.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="layout-spacing">
+    <ul>
+      <li>Seus favorecidos</li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  height: 50px;
+  background-color: var(--green-300);
+  color: var(--white);
+  display: flex;
+}
+
+ul {
+  list-style-type: none;
+  height: 100%;
+}
+
+li {
+  position: relative;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+
+li::after {
+  content: '';
+  display: block;
+  position: absolute;
+  bottom: 0;
+  width: 110%;
+  border-bottom: 3px solid var(--white);
+}
+</style>
