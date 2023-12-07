03eb40f76cc25994796b6a5ce66f374853578e0c
Matheus Henrique <matheus.181137@gmail.com>
2023-12-07 13:06:13 -0300

    feat: create input component

---
+++ src/components/Input/AppInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input v-bind="$attrs" placeholder="Nome, CPF, agência ou conta" />
+    <slot name="suffix"></slot>
+  </div>
+</template>
+
+<style scoped>
+div {
+  position: relative;
+}
+
+input {
+  font-size: var(--14px);
+  font-weight: 300;
+  color: var(--gray-500);
+  outline: none;
+  border: none;
+  border-radius: 3px;
+  padding-left: 5px;
+  width: 100%;
+}
+
+input::placeholder {
+  font-size: var(--14px);
+  font-weight: 300;
+}
+
+:slotted(.suffix) {
+  position: absolute;
+  top: 8px;
+  right: 10px;
+}
+</style>
