7d49d8577732b483d37ca4070bae0e34041d0728
Matheus Henrique <matheus.181137@gmail.com>
2023-12-07 13:07:33 -0300

    feat: add new tokens to theme

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import AppHeader from './components/Header/AppHeader.vue'
 import AppNavigation from './components/Navigation/AppNavigation.vue'
+import AppPageTitle from './components/PageTitle/AppPageTitle.vue'
 </script>
 
 <template>
   <AppHeader />
   <AppNavigation />
+  <AppPageTitle />
 </template>
