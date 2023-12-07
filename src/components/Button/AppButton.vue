18556a9bfaa4d3919d0d984c760d1fa08b25a8f8
Matheus Henrique <matheus.181137@gmail.com>
2023-12-07 13:07:11 -0300

    feat: create button component

---
+++ src/components/Button/AppButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+import PlusIcon from '../icons/PlusIcon.vue'
+</script>
+
+<template>
+  <button><PlusIcon /></button>
+</template>
+
+<style scoped>
+button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 41px;
+  height: 41px;
+  border-radius: 50%;
+  background-color: var(--green-300);
+}
+</style>
